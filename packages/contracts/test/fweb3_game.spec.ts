a137a50cd26cc6256efc44cff345dbe15053b065
Ryan <thefakeryan@icloud.com>
2022-11-25 12:02:30 -0800

    refactor contracts, configurations, general setup

--- packages/contracts/test/fweb3_game.spec.ts
+++ packages/contracts/test/fweb3_game.spec.ts
@@ -1,14 +1,23 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { Fweb3Game2, Fweb3Token } from './../typechain-types'
 
-let fweb3Game, fweb3Token
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
-describe("Fweb3Game", function () {
-  before(() => {
-    // Get the ContractFactory and Signers here.
+let fweb3Game: Fweb3Game2, fweb3Token: Fweb3Token, owner, user
 
+describe('Fweb3Game', () => {
+  beforeEach(async () => {
+    ;[owner, user] = await ethers.getSigners()
+    const TokenFactory = await ethers.getContractFactory('Fweb3Token')
+    fweb3Token = await TokenFactory.deploy()
+    await fweb3Token.deployed()
+
+    const GameFactory = await ethers.getContractFactory('Fweb3Game2')
+    fweb3Game = await GameFactory.deploy(fweb3Token.address)
+
+    await fweb3Game.deployed()
   })
-  it("Should test", async function () {
+  it('Compiles', async () => {
     expect(true)
-  });
-});
+  })
+})
