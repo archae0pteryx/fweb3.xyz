3f25fb4a839a347d5c6d4d0ba73a84d83b87718d
Ryan <thefakeryan@icloud.com>
2022-11-28 16:29:25 -0800

    clean up scripts

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,30 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+;(async () => {
+  try {
+    const [admin, user1] = await ethers.getSigners()
+    const adminAddress = await admin.getAddress()
+    const user1Address = await user1.getAddress()
+    console.log('Deploying contracts with the account:', adminAddress)
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+    const FweebFactory = await ethers.getContractFactory('FweebNFT')
+    const fweebNft = await FweebFactory.deploy()
+    await fweebNft.deployed()
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    console.log('FweebNFT deployed to:', fweebNft.address)
+    fweebNft.safeMint(user1Address, 'RIMRAF')
+  } catch (err) {
+    console.error(err)
+  }
+})()
--- packages/contracts/scripts/index.ts
+++ packages/contracts/scripts/index.ts
@@ -1,1 +1,22 @@
-// import { ethers } from 'hardhat'
+import { ethers } from 'hardhat'
+
+
+;(async () => {
+  try {
+
+    const [admin, user1] = await ethers.getSigners()
+    const adminAddress = await admin.getAddress()
+    const user1Address = await user1.getAddress()
+    console.log('Deploying contracts with the account:', adminAddress)
+
+    const FweebFactory = await ethers.getContractFactory('FweebNFT')
+    const fweebNft = await FweebFactory.deploy()
+    await fweebNft.deployed()
+
+    console.log('FweebNFT deployed to:', fweebNft.address)
+    fweebNft.safeMint(user1Address, 'RIMRAF')
+
+  } catch (err) {
+    console.error(err)
+  }
+})()
