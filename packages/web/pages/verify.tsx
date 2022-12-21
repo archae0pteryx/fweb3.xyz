8484813169dcec56ddd5c15a1374dc58648a893b
Ryan <thefakeryan@icloud.com>
2022-12-21 07:23:59 -0800

    add default export for verify page

--- packages/web/pages/verify.tsx
+++ packages/web/pages/verify.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from '@mui/material'
 
-export function VerifyPage() {
+export default function VerifyPage() {
   return (
     <Box>
       <Typography>We send a verification. Please check the ol&apos; mailbox</Typography>
